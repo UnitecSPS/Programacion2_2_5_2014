bbcd2bd364191e79c61adae5e1d8f3c13acb7459
Carlos Gochez <cgotcha@gmail.com>
2014-10-10 09:19:10 -0600

    Crear el repositorio de la clase de Progra 2

---
+++ src/Fechas.java
@@ -0,0 +1,21 @@
+
+import java.util.Calendar;
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Docente
+ */
+public class Fechas {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        c.set(1982,Calendar.AUGUST,10);
+        Date d = c.getTime();
+        System.out.println(d);
+    }
+}
