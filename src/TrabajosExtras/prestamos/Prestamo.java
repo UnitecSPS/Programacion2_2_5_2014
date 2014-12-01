7deec09ef9fd787e77f260fc0120c621de87255d
Raimmaster <rai.interiano@gmail.com>
2014-12-01 05:17:37 -0600

    Finalizando TG4-Extra

--- src/TrabajosExtras/prestamos/Prestamo.java
+++ src/TrabajosExtras/prestamos/Prestamo.java
@@ -7,6 +7,7 @@ package TrabajosExtras.prestamos;
 
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
@@ -19,6 +20,10 @@ public class Prestamo implements Loanable, Serializable{
     private Calendar fechaCreacion;
     private Calendar fechaMaxima;
     private double balance;
+
+    public int getCodigo() {
+        return codigo;
+    }    
     
     public Prestamo(int cod, String nom, double monto) throws LoanInvalidException{
         codigo = cod;
@@ -29,9 +34,10 @@ public class Prestamo implements Loanable, Serializable{
         montoTotal = monto;
         balance = monto;
         fechaCreacion = Calendar.getInstance();
+        fechaMaxima = Calendar.getInstance();
         fechaMaxima.set(fechaCreacion.get(Calendar.YEAR) + 5, 
                 fechaCreacion.get(Calendar.MONTH), 
-                fechaCreacion.get(Calendar.DATE));        
+                fechaCreacion.get(Calendar.DAY_OF_MONTH));        
     }
     
     @Override
@@ -48,7 +54,7 @@ public class Prestamo implements Loanable, Serializable{
         else
             balance -= monto;
         
-        System.out.printf("El balance actual es de L. %d\n", balance);
+        System.out.printf("El balance actual es de L. %.2f\n", balance);
     }
 
     @Override
@@ -57,16 +63,22 @@ public class Prestamo implements Loanable, Serializable{
     }
     
     public void print(){        
-        System.out.printf("Codigo: %d - Cliente: %s - Fecha de creacion %");
+        System.out.printf("Codigo: %d - Cliente: %s - Monto total: %.2f - " + 
+                "Balance: %.2f - Fecha de creacion: %tD - Fecha maxima para pagar: %tD  %s %n",
+                codigo, nombre, montoTotal, balance, fechaCreacion, fechaMaxima, 
+                new Date().getTime() > fechaMaxima.getTimeInMillis() ? " - Esta en mora" : "");
     }
     
     public void updateFechaMaxima(String date){
-        try{            
-            String [] datosFecha = date.split("/");
+        try{    
+            String [] datosFecha = date.split("/");   
+            int day = Integer.parseInt(datosFecha[0]);                             
+            int month = Integer.parseInt(datosFecha[1]); 
+            int year = Integer.parseInt(datosFecha[2]);                            
+            fechaMaxima.set(year, month, day);
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Fecha enviada con formato incorrecto.");
-        }
-            
+        }           
         
     }
 }
